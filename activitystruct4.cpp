0a8c8c78fac3e477daf14e00931c7c9dc7738002
SASCHADDD <saschadanu23@gmail.com>
2024-04-02 18:40:29 +0700

    end structure 4

--- activitystruct4.cpp
+++ activitystruct4.cpp
@@ -1,42 +1,47 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <string>
+using namespace  std;
 
-struct DetailAlamat
-{
+struct DetailAlamat {
     string desa;
     string kota;
 };
 
-struct mahasiswa
-{
+struct Mahasiswa {
     string nim;
     string nama;
     DetailAlamat alamat;
-    string umur;
+    int umur;
 };
- int main (){
-        mahasiswa mhs;
-        cout <<"Nomor Mahasiswa : ";
-        getline (cin,mhs.nim);
-        cout <<"Nama Mahasiswa :";
-        getline (cin,mhs.nama);
 
-        cout <<"Alamat Mahasiswa :"<<endl;
-        cout <<"\t Nama Desa : ";
-        cin >> mhs.alamat.desa;
+int main()
+{
+    Mahasiswa mhs[3];
+    for(int i = 0; i < 3; i++) {
+        cout << "Data ke-" << (i + 1) << ":" << endl;
+        cout << "Nomor Mahasiswa : ";
+        getline(cin, mhs[i].nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs[i].nama);
+
+        cout << "Alamat Mahasiswa : " << endl;
+        cout << "\t Nama Desa : ";
+        cin >> mhs[i].alamat.desa;
         cout << "\t Nama Kota : ";
-        cin >> mhs.alamat.kota;
-
-        cout <<"Umur Mahasiswa :";
-        cin >> mhs.umur;
-
-
-
-            cout<<endl; 
-            cout <<"\n NIM :"<< mhs.nim;
-            cout <<"\n Nama :"<< mhs.nama;
-            cout <<"\n Alamat :";
-            cout <<"\n \t Desa :"<< mhs.alamat.desa;
-            cout <<"\n \t Kota :"<< mhs.alamat.kota;
-            cout <<"\n Umur :"<< mhs.umur;
+        cin >> mhs[i].alamat.kota;
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs[i].umur;
+        cin.ignore(1,'\n');
+        cout << endl;
     }
+    for (int i = 0; i < 3; i++) {
+        cout << endl;
+        cout << "Data Mahasiswa ke-" << (i + 1) << ":" << endl;
+        cout << "\n NIM : " << mhs[i].nim;
+        cout << "\n Nama : " << mhs[i].nama;
+        cout << "\n Alamat : ";
+        cout << "\n \t Desa : " << mhs[i].alamat.desa;
+        cout << "\n \t Kota : " << mhs[i].alamat.kota;
+        cout << "\n Umur : " << mhs[i].umur;
+    }
+}
